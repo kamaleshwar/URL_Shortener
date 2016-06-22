8b6fb87c7f46488be19f6e683d8af6e9bc3b2dea
Kamal <panapakam.kamaleshwar@gmail.com>
2016-06-22 13:42:15 -0400

    Working code with few UI enhancements left

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+var crypto = require('crypto');
+
+urlMod = require('./models/url-mapping');
+mongoose.connect('mongodb://localhost/url-shortener');
+var db = mongoose.connection;
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Serve the files
+app.use(express.static('./public'));
+
+
+app.get('/:_url', function(req, res) {				
+		urlMod.getLongURL( req.params._url, function(err, response) {
+		if(err) {
+			throw err;
+		}
+		
+		var resultURL = "http://";
+		resultURL += response;
+		res.redirect(resultURL);
+	});	
+		
+});
+	
+app.post('/', function(req, res) {	
+	var entryObj = req.body;		
+	var limit = 7;		
+	var urlLength = entryObj.longURL.length;
+	var randomValueHex	= function(seed, limit) {
+						    return crypto.randomBytes(Math.ceil(seed/2))
+						        .toString('hex') // convert to hexadecimal format
+						        .slice(0,limit);   // return required number of characters
+							}
+	var shortURL = randomValueHex(urlLength, limit);
+	// var result = "http://localhost:3000/"						
+	
+	urlMod.addURL(entryObj, shortURL, function(err, entryObj) {
+		if(err) {
+			throw err;
+		}
+		var slashedURL = "localhost:3000/";
+		slashedURL += shortURL;
+
+		res.json(slashedURL);
+	});
+});
+
+
+app.listen(3000);
+
+console.log('server running on port 3000');
+
+module.exports = app;
